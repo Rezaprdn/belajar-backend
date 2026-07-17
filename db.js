3bfa9d12fe49bb40c723f8a9ac592097e2ed1518
Reza Herdiasyah <rezaherdiansyah76@gmail.com>
2026-07-17 14:32:59 +0700

    feat: CORS + koneksi PostgreSQL

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
     database: 'belajar_backend',
-    password: 'postgres',
+    password: 'riskaajaudah21',
     port: 5432,
 })
 
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const pool = require('./db')
 const app = express()
+const cors = require('cors')
+app.use(cors())
 
 app.use(express.json())
 
